9f84305c514145248c082e36a56a8462b7f6d2f0
Stefan Erwin Baumer <baumeredv@gmail.com>
2024-05-13 20:01:52 +0200

    feat: vertical scrolling in the log

--- featureTracking.md
+++ featureTracking.md
@@ -1,15 +1,18 @@
 # Feature Roadmap and Tracking
 
 ## Completed
-Nothing
+- open file from provided path
+- scroll vertically inside file
 
 ## Vision / Roadmap
 - display navigable view of text file from disk 
 - filter which lines are displayed with user-defined regex read from a provided file
 - color lines matching user-defined regex and color instructions
+- show keybind help info
 - toggle filters live in the viewer
 - simple live text filter
 - edit filters in the application and persist them
+- shortcut customization
 - support stdio rather than just a file flag
 - support live filtering for files "trickling in" through stdio
 - performance increase through multithreading
@@ -17,8 +20,7 @@ Nothing
 
 
 ## Roadstops / Immediate Goals
-- make that file navigable in the TUI as a single screen application
-  - ratatui with crossterm backend for windows support
+- make that file horizontally navigable in the TUI as a single screen application
 - load filters from a file
 - apply filters
 - add coloring to filters and display those colors
@@ -28,5 +30,5 @@ Nothing
 - github action builds
 - nix derivation
 - move command line arguments to clap
-- figure out if the ratatui demo way is actually the correct way to render - seems like a lot of resource. possibly use callback on change instead?
+- figure out if the ratatui demo way is actually the correct way to render - seems like a lot of resources. Possibly use callback on change instead?
 - graciously exit and tear down the terminal modifications if there is a panic
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,22 @@ use crossterm::{
 };
 use ratatui::{
     prelude::{CrosstermBackend, Stylize, Terminal},
+    text::Line,
     widgets::{self, Paragraph},
 };
-use std::{fs, io::stdout};
+use std::{
+    cmp::{max, min},
+    fs,
+    io::stdout,
+    usize,
+};
 
 fn main() -> Result<(), String> {
     let invocation_configuration = parse_args(std::env::args())?;
     let log = open_and_parse_log(&invocation_configuration.target_logfile)?;
-    let log: String = log
-        .lines()
-        .filter(|s| s.contains("bub"))
-        .map(|s| s.to_owned() + "\n")
-        .collect();
+    let mut model = Model::new(log.lines().collect());
 
+    // setup Ratatui
     stdout()
         .execute(EnterAlternateScreen)
         .map_err(|e| e.to_string())?;
@@ -26,17 +29,39 @@ fn main() -> Result<(), String> {
     terminal.clear().map_err(|e| e.to_string())?;
 
     loop {
+        // render
         terminal
             .draw(|frame| {
                 let area = frame.size();
-                frame.render_widget(Paragraph::new(&*log), area);
+                frame.render_widget(
+                    Paragraph::new(
+                        model
+                            .get_screen_slice(area.height.into())
+                            .iter()
+                            .map(|line| Line::from(*line))
+                            .collect::<Vec<_>>(),
+                    ),
+                    area,
+                );
             })
             .map_err(|e| e.to_string())?;
 
-        if event::poll(std::time::Duration::from_millis(16)).map_err(|e| e.to_string())? {
+        // handle input
+        if event::poll(std::time::Duration::from_millis(200)).map_err(|e| e.to_string())? {
             if let event::Event::Key(key) = event::read().map_err(|e| e.to_string())? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q' | 'Q') => break,
+                        KeyCode::Char('j') => model.scroll_lines_down(1),
+                        KeyCode::Char('k') => model.scroll_lines_up(1),
+                        KeyCode::Char('d') => model.scroll_lines_down(
+                            terminal.size().map_or(0, |area| (area.height / 2).into()),
+                        ),
+                        KeyCode::Char('u') => model.scroll_lines_up(
+                            terminal.size().map_or(0, |area| (area.height / 2).into()),
+                        ),
+                        _ => {}
+                    };
                 }
             }
         }
@@ -67,4 +92,35 @@ fn open_and_parse_log(path: &str) -> Result<String, String> {
 
 struct InvocationConfiguration {
     target_logfile: String,
+}
+
+#[derive(Default)]
+struct Model<'a> {
+    log: Vec<&'a str>,
+    text_offset: usize,
+}
+
+impl<'a> Model<'a> {
+    fn new(log: Vec<&'a str>) -> Self {
+        Self {
+            log,
+            text_offset: 0,
+        }
+    }
+
+    fn get_screen_slice(&self, length: usize) -> &[&str] {
+        let start = min(self.log.len(), self.text_offset);
+        let end = min(self.log.len(), self.text_offset + length);
+        &self.log[start..end]
+    }
+
+    fn scroll_lines_up(&mut self, amount: usize) {
+        let amount = min(amount, self.text_offset);
+        self.text_offset -= amount;
+    }
+
+    fn scroll_lines_down(&mut self, amount: usize) {
+        let amount = min(amount, usize::MAX - self.text_offset);
+        self.text_offset = min(max(self.log.len() - 1, 0), self.text_offset + amount);
+    }
 }
